e2fe88408811586c54e13691e2833715d2ff4c2d
krukkrz <40073114+krukkrz@users.noreply.github.com>
2026-01-09 16:06:16 +0100

    Update MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,9 +5,9 @@ const MainPage = () => {
     return (
         <div className="flex flex-col bg-[#FFD557]">
             <div className="max-w-[1000px] mx-auto flex flex-col gap-10 items-center mt-20 text-center">
-                <h1 className="font-black text-[40px] md:text-[90px]">Maciej Palmowski</h1>
+                <h1 className="font-black text-[40px] md:text-[90px]">Mick Palmowski</h1>
                 <p className="text-[20px]">
-                    It's Mick here! Lodz Filmschool graduate with over 10 years of experience in editing variety forms of videos :)
+                    It's Mick here - Lodz Filmschool graduate with over 10 years of experience in editing variety forms of videos.
                 </p>
                 <SectionList portfolio={portfolio}/>
             </div>
